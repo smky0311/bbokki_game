1892996b3f7fb5f656a255739dbefe5756b6b885
smky0311 <std08219@gmail.com>
2025-11-30 01:13:21 +0900

    utf8

--- bbokki_game.client/src/App.jsx
+++ bbokki_game.client/src/App.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // ---------------------------------------------------------
 // 아이콘 컴포넌트
